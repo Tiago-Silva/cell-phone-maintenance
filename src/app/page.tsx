2ffb833ed735ab95e13b2634997edb328edd1c06
tiago <ti.almeida58@gmail.com>
2025-02-28 20:57:30 -0300

    refactor: using header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from "./page.module.css";
+import Header from "@/app/components/header/header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Header />
       <main className={styles.main}>
       </main>
     </div>
