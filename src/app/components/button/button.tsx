a70711a8d3e5cdd620dfb22fda8a620e61e345b2
tiago <ti.almeida58@gmail.com>
2025-03-01 09:59:53 -0300

    feat: creating button.tsx

---
+++ src/app/components/button/button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './button.module.css';
+
+const Button = () => {
+    return (
+        <button className={styles.container}>
+            Garantir minha vaga
+        </button>
+    );
+};
+
+export default Button;
