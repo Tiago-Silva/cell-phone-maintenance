dc276f614c4530c4fbfdde2921f65bea507410ee
tiago <ti.almeida58@gmail.com>
2025-02-28 21:52:09 -0300

    refactor: importing and using HomeSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import Header from "@/app/components/header/header";
+import HomeSection from "@/app/components/home/homeSection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
       <main className={styles.main}>
+          <HomeSection />
       </main>
     </div>
   );
