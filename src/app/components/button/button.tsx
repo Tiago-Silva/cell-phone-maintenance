3ea46f4a5191bdb8b2034f57c216169a3d6672e1
tiago <ti.almeida58@gmail.com>
2025-03-04 20:35:38 -0300

    refactor: adding attributes to be more generic

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './button.module.css';
+import {IconType} from "react-icons";
 
-const Button = () => {
+interface Props {
+    title: string;
+    icon?: IconType;
+    width?: string;
+    height?: string;
+}
+
+const Button: React.FC<Props> = ({
+    title,
+    icon: Icon,
+    width = '35rem',
+    height = '6rem',
+}) => {
     return (
-        <button className={styles.container}>
-            Garantir minha vaga
+        <button className={styles.container} style={{ width, height }}>
+            {title}
+            {Icon && <Icon />}
         </button>
     );
 };
