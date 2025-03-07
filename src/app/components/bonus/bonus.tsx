e7245d74f1289166b7f9d6645d4b1ad4383d56ff
tiago <ti.almeida58@gmail.com>
2025-03-07 14:45:55 -0300

    refactor: removing duplicating

--- src/app/components/bonus/bonus.tsx
+++ src/app/components/bonus/bonus.tsx
@@ -9,7 +9,7 @@ const Bonus = () => {
             <h3 className={styles.title}>4 bônus exclusivos</h3>
 
             <div className={styles.card}>
-                <div className={styles.bonusText}>
+                <div className={`${styles.bonusText} greenEndLine`}>
                     <h2>Curso Introdução ao WordPress</h2>
                     <h3>Curso completo de WordPress, passando por toda sua base e menus para que você não tenha dúvidas
                         sobre a ferramenta e consiga evoluir.</h3>
@@ -31,7 +31,7 @@ const Bonus = () => {
                     height={170}
                     className={styles.bonusImage}
                 />
-                <div className={styles.bonusText}>
+                <div className={`${styles.bonusText} greenEndLine`}>
                     <h2>Curso Introdução ao Elementor</h2>
                     <h3>Curso completo de Elementor, te apresentando toda a base desse plugin, passando pelos seus menus
                         e funcionalidades e como ele pode te ajudar no dia a dia.</h3>
@@ -39,7 +39,7 @@ const Bonus = () => {
             </div>
 
             <div className={styles.card}>
-                <div className={styles.bonusText}>
+                <div className={`${styles.bonusText} greenEndLine`}>
                     <h2>1 licença do Elementor PRO</h2>
                     <h3>Uma licença do Elementor PRO, para que você não precise gastar mais nada com isso para colocar
                         seus estudos em prática. Licença válida enquanto durar o curso.</h3>
@@ -61,7 +61,7 @@ const Bonus = () => {
                     height={170}
                     className={styles.bonusImage}
                 />
-                <div className={styles.bonusText}>
+                <div className={`${styles.bonusText} greenEndLine`}>
                     <h2>1 licença do Happy Addons</h2>
                     <h3>Uma licença do Happy Addons, um plugin incrível de complementos para o Elementor para você criar
                         páginas ainda mais bonitas e impactantes.</h3>
