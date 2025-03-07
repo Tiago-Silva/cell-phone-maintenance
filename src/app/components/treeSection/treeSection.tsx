57ca6d000de57ba13064ab1c9a22363afc71b815
tiago <ti.almeida58@gmail.com>
2025-03-07 14:39:22 -0300

    refactor: applying responsiveness

--- src/app/components/treeSection/treeSection.tsx
+++ src/app/components/treeSection/treeSection.tsx
@@ -30,34 +30,35 @@ const TreeSection = () => {
         <section className={`${styles.container} greenStartLine greenEndLine`} id={'treeSection'}>
             <h2 className={styles.title}>Conheça tudo que você vai aprender no curso</h2>
 
-            <Swiper
-                modules={[Navigation]}
-                spaceBetween={0}
-                slidesPerView={6}
-                navigation={true}
-                loop={true}
-                breakpoints={{
-                    320: { slidesPerView: 1 },
-                    480: { slidesPerView: 2 },
-                    640: { slidesPerView: 3 },
-                    768: { slidesPerView: 4 },
-                    1024: { slidesPerView: 5 },
-                    1280: { slidesPerView: 6 },
-                }}
-                className={styles.swiperContainer}
-            >
-                {images.map((image, index) => (
-                    <SwiperSlide key={index} className={styles.slide}>
-                        <Image
-                            src={`/module/${image}`}
-                            alt={`Módulo ${index + 1}`}
-                            width={200}
-                            height={300}
-                            className={styles.image}
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            <div className={styles.swiperContainer}>
+                <Swiper
+                    modules={[Navigation]}
+                    spaceBetween={10}
+                    slidesPerView={6}
+                    navigation={true}
+                    loop={true}
+                    breakpoints={{
+                        320: { slidesPerView: 1 },
+                        480: { slidesPerView: 2 },
+                        640: { slidesPerView: 3 },
+                        768: { slidesPerView: 4 },
+                        1024: { slidesPerView: 5 },
+                        1280: { slidesPerView: 6 },
+                    }}
+                >
+                    {images.map((image, index) => (
+                        <SwiperSlide key={index} className={styles.slide}>
+                            <Image
+                                src={`/module/${image}`}
+                                alt={`Módulo ${index + 1}`}
+                                width={200}
+                                height={300}
+                                className={styles.image}
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
 
             <div className={styles.bonus}>+3 bônus incríveis!</div>
 
